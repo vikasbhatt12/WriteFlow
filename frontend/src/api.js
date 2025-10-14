c110d08b541417f1cfa6e028c96140aceb305aa3
vikasbhatt12 <vikasbhatt12082003@gmail.com>
2025-10-15 02:23:09 +0530

    updated the url for backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 
-const API_BASE_URL = 'http://localhost:5001/api';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5001/api';
 
 
 const getConfig = () => {
